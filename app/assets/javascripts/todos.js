ba72681a9864a8e1acb7d77fb6a68862fc5bbe7d
Stephen Achurra <achurra7@gmail.com>
2017-08-21 10:15:40 -0700

    Add toggle more info

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,9 +1,13 @@
 const newSection = document.querySelector(".new");
 const editSection = document.querySelector(".edit");
+
+const editButtons = document.querySelectorAll(".edit-button");
 const editForm = document.querySelector(".edit-form");
 const titleField = editForm.querySelector(".edit-form input[name='todo[title]']");
 const dueDateField = editForm.querySelector(".edit-form input[name='todo[due_date]']");
 const detailsField = editForm.querySelector(".edit-form textarea[name='todo[details]']");
+
+const moreToggles = document.querySelectorAll(".more-toggle");
 
 function setupEditForm(id, title, due_date, details) {
   newSection.classList.add("hidden");
@@ -15,14 +19,38 @@ function setupEditForm(id, title, due_date, details) {
   editForm.action = `${window.location.origin}/todos/${id}`
 }
 
+function createEditForm(e) {
+  newSection.classList.add("hidden");
+  editSection.classList.remove("hidden");
+
+  const todo = this.parentElement.parentElement;
+
+  titleField.value = todo.querySelector('.title').innerHTML
+  dueDateField.value = todo.querySelector('.due-date').innerHTML;
+  detailsField.value = (todo.querySelector('.details').innerHTML).replace(/<br>/g, "\r");
+  editForm.action = `${window.location.origin}/todos/${parseInt(todo.id)}`
+}
+
 function cancelEdit(e) {
   e.preventDefaults();
   newSection.classList.remove("hidden");
   editSection.classList.add("hidden");
 }
 
+function moreToggle(e) {
+  todo = parseInt(this.parentElement.parentElement.id);
+  this.innerHTML == "More..." ? this.innerHTML = "Hide..." : this.innerHTML = "More..."
+  document.getElementById(`${todo}`)
+    .querySelector(".more-info")
+    .classList.toggle("hidden");
+}
+
 function cleanURL() {
   window.history.pushState("","",window.location.origin);
 }
+
+moreToggles.forEach((toggle) => toggle.addEventListener("click", moreToggle));
+editButtons.forEach((button) => button.addEventListener("click", createEditForm));
+document.querySelector(".cancel-edit").addEventListener("click", cancelEdit);
 
 document.addEventListener("DOMContentLoaded", cleanURL);
